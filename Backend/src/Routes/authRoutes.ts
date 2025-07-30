b76bf0e7bcd84de419cd5403771d43f6a3e6e762
Abhishek Tiwari <abhishektiwari4843@email.com>
2025-07-30 12:31:31 +0530

    change in authroutes

--- Backend/src/Routes/authRoutes.ts
+++ Backend/src/Routes/authRoutes.ts
@@ -5,6 +5,6 @@ import {upload,addProduct} from '../controllers/ProductController';
 const router = express.Router();
 
 router.get('/test', text);
-router.post('/products', upload.single('image'), addProduct);
+router.post('/products', upload.single('image'), addProduct );
 
 export default router;
